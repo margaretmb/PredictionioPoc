ff4c37a5310903a6cae56775c702481987825dd8
Margaret McNulty-Beldyk <margaretmb@weareratio.com>
2013-12-06 13:47:53 -0800

    some mock data

---
+++ src/main/java/com/neon/predictionioPoc/Importer.java
@@ -0,0 +1,65 @@
+package com.neon.predictionioPoc;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Importer {
+
+	public static Set<String> generateMockUids() {
+		Set<String> set = new TreeSet<>();
+		set.add("1");
+		set.add("2");
+		set.add("3");
+		set.add("4");
+		return set;
+	}
+
+
+	public static Map<String, Set<String>> generateMockWatchedMovies() {
+		Map<String, Set<String>> map = new HashMap<>();
+		Set<String> movies1 = new TreeSet<>();
+		movies1.add("1");
+		
+		Set<String> movies2 = new TreeSet<>();
+		movies2.add("1");
+		movies2.add("2");
+		movies2.add("7");
+		movies2.add("5");
+		movies2.add("3");
+		
+		Set<String> movies3 = new TreeSet<>();
+		movies3.add("1");
+		movies3.add("4");
+		movies3.add("5");
+		movies3.add("3");
+		
+		Set<String> movies4 = new TreeSet<>();
+		movies4.add("1");
+		movies4.add("4");
+		
+		map.put("1", movies1);
+		map.put("2", movies2);
+		map.put("3", movies3);
+		map.put("4", movies4);
+		
+		return map;
+	}
+
+	public static Map<String, String> generateMockMovies() {
+		Map<String, String> map = new HashMap<>();
+		map.put("1", "Titanic");
+		map.put("2", "Christmas Story");
+		map.put("3", "Lord of the Rings");
+		map.put("4", "Harry Potter");
+		map.put("5", "Iron Man");
+		map.put("6", "Shrek");
+		map.put("7", "Fakey McFakeMovie");
+		map.put("8", "Emmet Otter's Jug Band Christmas");
+		map.put("9", "Treasure Planet");
+		map.put("10", "Plan 9 From Outer Space");
+		return map;
+	}
+
+}
--- src/test/java/com/neon/predictionioPoc/PredictionTest.java
+++ src/test/java/com/neon/predictionioPoc/PredictionTest.java
@@ -1,25 +1,54 @@
 package com.neon.predictionioPoc;
 
 import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.Set;
+
 import io.prediction.Client;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PredictionTest {
-	
-	private Client client;
-	
+
+	private static Client client;
+	private static Importer importer;
+
 	@BeforeClass
 	public static void before() {
-		// do stuff
+		String apiKey = "31biODHABXLSrrf76E3I4Ll0pO0v3s59BdOOr92mimQQuZU0gCnzXl8vTYsOOG8s";
+		String url = "http://localhost:8000";
+		client = new Client(apiKey, url);
+		importer = new Importer();
+	}
+
+	@Test
+	public void ImporterShouldGenerateSomeIds() {
+
+		Set<String> uids = Importer.generateMockUids();
+		assertNotNull(uids);
+		assertTrue(uids.size() == 4);
+		assertTrue(uids.contains("1"));
+		assertFalse(uids.contains("72"));
+		
 	}
 	
 	@Test
-	public void clientShouldImportData() {
-		
-		fail("This is not a test");
-		
+	public void ImportShouldGenerateSomeMoviesToo() {
+		Map<String, String> movies = Importer.generateMockMovies();
+		assertNotNull(movies);
+		assertTrue(movies.size() > 0);
+		assertTrue(movies.get("6").equals("Shrek"));
+		assertFalse(movies.get("8").equals("Wrong Movie Title"));
+	}
+	
+	@Test
+	public void InputShouldGenerateSomeMockWatchedMovies() {
+		Map<String, Set<String>> watchedMovies = Importer.generateMockWatchedMovies();
+		assertNotNull(watchedMovies);
+		assertFalse(watchedMovies.size() == 0);
+		assertTrue(watchedMovies.get("3").contains("4"));
 	}
 
 }
