f39390525928e5bf733e5eb9ff00f097477f6d18
Margaret McNulty-Beldyk <margaretmb@weareratio.com>
2013-12-06 12:35:48 -0800

    first commit

---
+++ src/test/java/com/neon/predictionioPoc/PredictionTest.java
@@ -0,0 +1,25 @@
+package com.neon.predictionioPoc;
+
+import static org.junit.Assert.*;
+import io.prediction.Client;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PredictionTest {
+	
+	private Client client;
+	
+	@BeforeClass
+	public static void before() {
+		// do stuff
+	}
+	
+	@Test
+	public void clientShouldImportData() {
+		
+		fail("This is not a test");
+		
+	}
+
+}
