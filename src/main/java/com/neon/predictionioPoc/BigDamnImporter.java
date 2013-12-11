46dc0a0153a35fec42ff884eaecdf5c4ec034036
Margaret McNulty-Beldyk <margaretmb@weareratio.com>
2013-12-11 15:47:11 -0800

    stuff

--- src/main/java/com/neon/predictionioPoc/BigDamnImporter.java
+++ src/main/java/com/neon/predictionioPoc/BigDamnImporter.java
@@ -2,10 +2,14 @@ package com.neon.predictionioPoc;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -18,20 +22,70 @@ public class BigDamnImporter {
 		try {
 			in = new FileReader(file);
 			BufferedReader reader = new BufferedReader(in);
-			
+
 			String line = reader.readLine();
 			while (line != null) {
 				String[] splitLine = line.split("\\|");
 				users.add(splitLine[0]);
 				line = reader.readLine();
 			}
-			
+
 			in.close();
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
 		return users;
+	}
+
+	public Map<String, String> importMovies() {
+		Map<String, String> movies = new HashMap<>();
+
+		try {
+			File file = new File("src/main/resources/ml-100k/u.item");
+			FileReader in = new FileReader(file);
+			BufferedReader reader = new BufferedReader(in);
+
+			String line = reader.readLine();
+			while (line != null) {
+				String[] splitLine = line.split("\\|");
+				movies.put(splitLine[0], splitLine[1]);
+				line = reader.readLine();
+			}
+
+			reader.close();
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return movies;
+	}
+
+	public List<String[]> importRatings() {
+		List<String[]> ratings = new ArrayList<>();
+
+		try {
+			File file = new File("src/main/resources/ml-100k/u.data");
+			FileReader in = new FileReader(file);
+			BufferedReader reader = new BufferedReader(in);
+			
+			String line = reader.readLine();
+			while (line != null) {
+				String[] splitLine = line.split("\t");
+				String[] subArray = Arrays.copyOfRange(splitLine, 0, 3);
+				
+				ratings.add(subArray);
+				
+				line = reader.readLine();
+			}
+			
+			in.close();
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ratings;
 	}
 
 }
--- src/test/java/com/neon/predictionioPoc/BigDamnImportTest.java
+++ src/test/java/com/neon/predictionioPoc/BigDamnImportTest.java
@@ -2,9 +2,19 @@ package com.neon.predictionioPoc;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import io.prediction.Client;
+import io.prediction.CreateItemRequestBuilder;
+import io.prediction.FutureAPIResponse;
+import io.prediction.ItemRecGetTopNRequestBuilder;
+import io.prediction.ItemSimGetTopNRequestBuilder;
+import io.prediction.UnidentifiedUserException;
+import io.prediction.UserActionItemRequestBuilder;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,20 +22,110 @@ import org.junit.Test;
 public class BigDamnImportTest {
 	
 	private static Client client;
+	private static BigDamnImporter importer;
+	private static String apiKey;
+	private static String url;
 
 	@BeforeClass
 	public static void before() {
-		String apiKey = "J00zzxkKUHDXKN89x8snwKe3qoRWzFiqWBVIEalP37Udx79InfvkLwIAVxM5c6Hb";
-		String url = "http://localhost:8000";
+		apiKey = "cvjMdOrvlQPY0CmQn7yBArK5BP5YpD0vFpJIyFJ3wKYfGqek6yV5QkYHcpijOoii";
+		url = "http://localhost:8000";
 		client = new Client(apiKey, url);
+		importer = new BigDamnImporter();
 	}
 	
 	@Test
-	public void shouldImportSomeUsers() {
-		BigDamnImporter importer = new BigDamnImporter();
+	public void clientShouldImportAllTheseNiftyThings() throws ExecutionException, InterruptedException, IOException, UnidentifiedUserException {
 		Set<String> users = importer.importUsers();
-		assertNotNull(users);
-		assertFalse(users.size() == 0);
+		for (String uid : users) {
+			client.createUser(uid);
+			System.out.println("adding user " + uid);
+		}
+		
+		Map<String, String> movies = importer.importMovies();
+		String[] type = new String[1];
+		type[0] = "movie";
+		
+		for (String movie : movies.keySet()) {
+			client.createItem(movie, type);
+			System.out.println("adding movie " + movies.get(movie));
+		}
+		
+		List<String[]> ratings = importer.importRatings();
+		
+		for (String[] ratingArray : ratings) {
+			String ratingUser = ratingArray[0];
+			client.identify(ratingUser);
+			String ratedMovie = ratingArray[1];
+			String rating = ratingArray[2];
+			UserActionItemRequestBuilder builder = new UserActionItemRequestBuilder(url, "json", apiKey, "rate", ratingUser, ratedMovie);
+			builder.rate(Integer.parseInt(rating));
+			client.userActionItem(builder);
+			client.userActionItem("view", ratedMovie);
+			System.out.println("user " + ratingUser + " viewed " + movies.get(ratedMovie) + " and rated it " + rating + " stars.");
+		}
+	}
+	
+	@Test
+	public void clientShouldRecommendAllTheThings() throws UnidentifiedUserException, ExecutionException, InterruptedException, IOException {
+		String id = "999";
+		Map<String, String> movies = importer.importMovies();
+		List<String[]> ratings = importer.importRatings();
+		
+		for (String [] arr : ratings) {
+			if (arr[0].equals(id)) {
+				String movie = movies.get(arr[1]);
+				String rating = arr[2];
+				System.out.println("user " + id  + " rated " + movie + " " + rating + " stars." );
+			}
+		}
+		
+		System.out.println("");
+		
+		client.identify(id);
+		ItemRecGetTopNRequestBuilder builder = client.getItemRecGetTopNRequestBuilder("BigDamnEngine", 10);
+		String[] recs = client.getItemRecTopN(builder);
+				
+		for (String rec : recs) {
+			String title = movies.get(rec);
+			System.out.println("user " + id + " might like " + title);
+		}
+		
+		
+	}
+	
+	@Test
+	public void clientShouldRecommendThingsToWatch() throws UnidentifiedUserException, ExecutionException, InterruptedException, IOException {
+		
+		String id = "999";
+		Map<String, String> movies = importer.importMovies();
+		
+		client.identify(id);
+		ItemRecGetTopNRequestBuilder builder = client.getItemRecGetTopNRequestBuilder("WatchEngine", 10);
+		String[] recs = client.getItemRecTopN(builder);
+				
+		for (String rec : recs) {
+			String title = movies.get(rec);
+			System.out.println("user " + id + " could watch " + title);
+		}
+		
+	}
+	
+	@Test
+	public void clientShouldDoStuffWithTheOtherEngineToo() throws ExecutionException, InterruptedException, IOException {
+		String[] stuff = 
+				client.getItemSimTopN(new ItemSimGetTopNRequestBuilder(url, "json", 
+						apiKey, "similarityEngine", "28", 10));
+		
+		Map<String, String> movies = importer.importMovies();
+		
+		System.out.println("Stuff similar to Apollo 13: ");
+		
+		for (String thing : stuff) {
+			String title = movies.get(thing);
+			System.out.println(title);
+		}
+		
 	}
 
 }
